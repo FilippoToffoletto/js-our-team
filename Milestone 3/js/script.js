9ff5c8584c1f3285da0665dc6f70ad5f4ed770a7
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-25 16:03:00 +0200

    [add]milestone3

---
+++ Milestone 3/js/script.js
@@ -0,0 +1,54 @@
+// banca dati
+const team = [
+    {
+        nome : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        profile : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome : 'Angela Caroll',
+        role : 'Chief Editor',
+        profile : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome : 'Walter Gordon',
+        role : 'Office Manager',
+        profile : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome : 'Angela Lopez',
+        role : 'Social Media Manager',
+        profile : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome : 'Scott Estrada',
+        role : 'Developer',
+        profile : 'scott-estrada-developer.jpg'
+    },
+    {
+        nome : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        profile : 'barbara-ramos-graphic-designer.jpg'
+    },
+];
+
+const lista = document.querySelector('.row');
+
+for(let indice in team){
+    const member = team[indice];
+    const output = `
+<div class="col-lg-4 col-sm-6 py-4">
+        <div class="card overflow-hidden" style="width: 18rem;">
+        <div class="overflow-hidden">
+            <img src="../img/${member.profile}" class="card-img-top" alt="${member.nome}">
+        </div>
+            <div class="card-body">
+            <h4>${member.nome}</h4>
+            <h5>${member.role}</h5>
+            </div>
+        </div>
+</div>`;
+    console.log(output);
+    lista.innerHTML  += output;
+}
+
