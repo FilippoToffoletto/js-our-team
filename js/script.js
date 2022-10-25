3ca254f205238da2b81914014a131fad43765c8c
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-25 15:07:26 +0200

    [add]milestone 0

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// banca dati
+const teams = [
+    {
+        nome : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        profile : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome : 'Angela Caroll',
+        role : 'Chief Editor',
+        profile : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome : 'Walter Gordon',
+        role : 'Office Manager',
+        profile : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome : 'Angela Lopez',
+        role : 'Social Media Manager',
+        profile : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome : 'Scott Estrada',
+        role : 'Developer',
+        profile : 'scott-estrada-developer.jpg'
+    },
+    {
+        nome : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        profile : 'barbara-ramos-graphic-designer.jpg'
+    },
+];
+
+console.log(teams);
